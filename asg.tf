06ccffd21c5602ab6af4d2bcf6bbb36123f0b01b
John Blanford <johnb@peakserv.com>
2020-10-21 16:30:06 -0700

    Add manual auto scaling group

---
+++ asg.tf
@@ -0,0 +1,48 @@
+resource "aws_launch_configuration" "web_lc" {
+  name_prefix     = "${var.project_name}-${var.environment}-lc-"
+  image_id        = var.web_server_ami
+  instance_type   = var.web_server_instance_type
+
+  security_groups             = [aws_security_group.public-web_server.id]
+  iam_instance_profile        = aws_iam_instance_profile.ssm_profile.name
+  associate_public_ip_address = false
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "web_asg" {
+  name_prefix          = "${var.project_name}-${var.environment}-asg-"
+  launch_configuration = aws_launch_configuration.web_lc.name
+  vpc_zone_identifier  = module.vpc.private_subnets
+
+  desired_capacity     = 3
+  min_size             = 1
+  max_size             = 9
+  health_check_type    = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-${var.environment}-web"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Project"
+    value               = var.project_name
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = var.environment
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
