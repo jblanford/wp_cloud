4504faf37821358394c3760b570fe336ce6790e3
John Blanford <johnb@peakserv.com>
2020-10-22 18:55:23 -0700

    Add info to default web page created by user data

--- user_data.sh
+++ user_data.sh
@@ -5,3 +5,9 @@ systemctl start httpd
 systemctl enable httpd
 
 echo '<h1>Default Page: Project = ${project_name} Environment = ${environment}</h1>' >> /var/www/html/index.html
+
+ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+HOST=`curl -s http://169.254.169.254/latest/meta-data/hostname`
+
+echo "</br>instance-id: $ID<br/>" >> /var/www/html/index.html
+echo "</br>hostname: $HOST<br/>" >> /var/www/html/index.html
