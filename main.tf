64dabdc7a609577b81045e85ca7276d2dd5d8d3f
John Blanford <johnb@peakserv.com>
2020-10-30 15:55:08 -0700

    Add vars to main tf setup

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region  = "us-east-2"
-  profile = "terraform"
+  region  = var.aws_region
+  profile = var.aws_profile
 }
 
 # lookup the current region
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "aws_region" {
+  description = "The AWS region name"
+  type        = string
+}
+
+variable "aws_profile" {
+  description = "The AWS cli profile name"
+  type        = string
+}
+
 variable "project_name" {
   description = "The name of this project"
   type        = string
